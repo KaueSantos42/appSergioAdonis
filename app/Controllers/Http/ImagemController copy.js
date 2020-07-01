38012d52599c85578b1f10155c304dd16beab39a
lorveder11 <56522709+lorveder11@users.noreply.github.com>
2020-07-01 20:58:49 -0300

    Create ImagemController copy.js

---
+++ app/Controllers/Http/ImagemController copy.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const Imovel = use('App/Models/Imovel');
+const Helpers = use('Helpers');
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+
+
+/**
+ * Resourceful controller for interacting with imovels
+ */
+class ImagemController {
+
+  async store ({ request, params}) {
+
+    const imovel = await Imovel.findOrFail(params.id);
+
+    const imagens = request.file('image', {
+      types: ['image'],
+      size: '5mb'
+    });
+
+    await imagens.moveAll(Helpers.tmpPath('uploads'), file => ({
+      name: `${Date.now()}-${file.clientName}`
+    }));
+
+    if(!imagens.movedAll()){
+      return imagens.errors();
+    }
+  }
+
+
+  async show ({ params, response }) {
+
+    const imagem = await Imovel.findOrFail(params.id);
+
+    return response.download(Helpers.tmpPath(`upload/${params.caminho}`));
+  }
+
+
+  async update ({ params, request, response }) {
+
+    const imovel = await Imovel.findOrFail(params.id);
+    const data = request.only(['descricao','endereco','valor','latitude','longitude']);
+    
+    imovel.merge(data);
+    await imovel.save();
+    
+    return imovel;
+  }
+
+  
+  async destroy ({ params, response, auth }) {
+    
+    const imovel = await Imovel.findOrFail(params.id);
+
+    if(imovel.user_id !== auth.user.id){
+      return response.status(401).send({error: 'voce nao tem autorizacao!'})
+    }
+    
+    await imovel.delete();
+  }
+}
+
+module.exports = ImovelController
